cc86104718ff22f0eefa3d46e15006825af410a3
Martyna Plawny <kotnik909@gmail.com>
2023-11-17 09:28:10 +0100

    commit after zadanie7.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,48 @@
-public class Main {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-
+class Main {
     public static void main(String[] args) {
-
+        readFile();
+        iterateAfterMap();
     }
 
+    static Map<Integer, Integer> mapOfNumbers = new HashMap<>();
+
+    static void iterateAfterMap() {
+        for (Map.Entry<Integer, Integer> entry : mapOfNumbers.entrySet()) {
+            System.out.println(entry.getKey() + " - liczba wystąpień " + entry.getValue());
+        }
+    }
+
+    static void readFile() {
+        String fileName = "liczby.txt";
+
+        FileReader fileReader;
+        BufferedReader reader = null;
+        try {
+            fileReader = new FileReader(fileName);
+            reader = new BufferedReader(fileReader);
+            String nextLine;
+            while ((nextLine = reader.readLine()) != null) {
+                if (mapOfNumbers.containsKey(Integer.valueOf(nextLine))) {
+                    mapOfNumbers.put(Integer.valueOf(nextLine), mapOfNumbers.get(Integer.valueOf(nextLine)) + 1);
+                } else {
+                    mapOfNumbers.put(Integer.valueOf(nextLine), 1);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
